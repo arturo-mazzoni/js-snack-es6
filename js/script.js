a666710114847d86438197ff5fd937701a600470
arturo <arturomazzoni@gmail.com>
2021-01-19 15:54:19 +0100

    snack 1 complete

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// ***********snack 1**********************************
+
+// Utilizzando const e/o let inserire un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+let n = parseInt(prompt("inserisci un numero"));
+
+if (n % 2 == 0){
+  console.log(n);
+} else {
+  let n = parseInt(prompt("inserisci un numero"));
+  console.log(n);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ***********snack 2**********************************
+
+// Utilizzando const e/o let e template literal. Creare due tag div con due id diversi.
+// Un div avrà il testo colorato di rosso mentre l’altro di verde.
+// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+
+
+
+
+// ***********snack 3**********************************
+
+// Utilizzando const e/o let. Dare la possibilità di inserire due parole.
+// Verificare se le due parole hanno la stessa lunghezza.
+// Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
