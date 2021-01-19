0c91e8846753182f7796e231d80d06df92be6dc0
arturo <arturomazzoni@gmail.com>
2021-01-19 17:16:06 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@
 
 // Utilizzando const e/o let inserire un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
-// let n = parseInt(prompt("inserisci un numero"));
-//
-// if (n % 2 == 0){
-//   console.log(n);
-// } else {
-//   let n = parseInt(prompt("inserisci un numero"));
-//   console.log(n);
-// }
+let n = parseInt(prompt("inserisci un numero"));
+
+if (n % 2 == 0){
+  console.log(n);
+} else {
+  n = parseInt(prompt("inserisci un numero"));
+  console.log(n);
+}
 
 
 // ***********snack 2**********************************
@@ -18,26 +18,25 @@
 // Un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
-const numeri = [11, 22, 33, 44, 55, 66, 77, 88, 99];
-const pari = [];
-const dispari = [];
-
-for (let i = 0; i < numeri.length; i++){
-  if (numeri[i] % 2 == 0){
-    pari.push(numeri[i]);
-  } else {
-    dispari.push(numeri[i]);
-  }
-}
-
-for (let i = 0; i < pari.length; i++){
-  document.getElementById("green").innerHTML += `${pari[i]}`;
-}
-for (let i = 0; i < dispari.length; i++){
-  document.getElementById("red").innerHTML += `${dispari[i]}`;
-}
-
-
+// const numeri = [11, 22, 33, 44, 55, 66, 77, 88, 99];
+// const pari = [];
+// const dispari = [];
+//
+// for (let i = 0; i < numeri.length; i++){
+//   if (numeri[i] % 2 == 0){
+//     pari.push(numeri[i]);
+//   } else {
+//     dispari.push(numeri[i]);
+//   }
+// }
+//
+// for (let i = 0; i < pari.length; i++){
+//   document.getElementById("green").innerHTML += `${pari[i]}`;
+// }
+//
+// for (let i = 0; i < dispari.length; i++){
+//   document.getElementById("red").innerHTML += `${dispari[i]}`;
+// }
 
 
 // ***********snack 3**********************************
@@ -46,8 +45,8 @@ for (let i = 0; i < dispari.length; i++){
 // Verificare se le due parole hanno la stessa lunghezza.
 // Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
-// let word1 = prompt("inserisci una parola");
-// let word2 = prompt("inserisci una parola");
+// const word1 = prompt("inserisci una parola");
+// const word2 = prompt("inserisci una parola");
 //
 // if (word1.length == word2.length){
 //   console.log(word1 + " " + word2);
@@ -56,26 +55,3 @@ for (let i = 0; i < dispari.length; i++){
 // }else {
 //   console.log(word1);
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// sdfgfg
